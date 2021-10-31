d6c5d53ef6fe0744ec345297963fba104701abba
rey-Adriano <2017110727@udh.edu.pe>
2021-10-31 11:17:18 -0500

    ds

--- aplicacion/modelo/Ubicacion.php
+++ aplicacion/modelo/Ubicacion.php
@@ -1,7 +1,20 @@
 <?php
+
+
+namespace aplicacion\modelo; 
+
+use config\Conexion; 
+
+require_once "config/Conexion.php"; 
+
+
 class Ubicacion{
 private $id;
 private $calle;
 private $provincia; 
+private $distrito; 
+private $nro; 
+private $pedido_id; 
+
 
 }
